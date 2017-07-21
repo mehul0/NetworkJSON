f76a4f3a2e94b4459e24ffcb1c338b0b254568ed
Mehul solanki <mehul2792@gmail.com>
2017-07-21 14:55:24 +0530

    JSON Parshing with Volley is Done

--- app/src/main/java/com/pinaksoftwares/networkjson/volley/adapter/CustomListAdapter.java
+++ app/src/main/java/com/pinaksoftwares/networkjson/volley/adapter/CustomListAdapter.java
@@ -22,39 +22,36 @@ public class CustomListAdapter extends BaseAdapter {
     private LayoutInflater inflater;
     private List<Movie> movieItems;
     ImageLoader imageLoader = AppController.getInstance().getImageLoader();
- 
+
     public CustomListAdapter(Activity activity, List<Movie> movieItems) {
         this.activity = activity;
         this.movieItems = movieItems;
     }
- 
+
     @Override
     public int getCount() {
         return movieItems.size();
     }
- 
+
     @Override
     public Object getItem(int location) {
         return movieItems.get(location);
     }
- 
+
     @Override
     public long getItemId(int position) {
         return position;
     }
- 
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
- 
+
         if (inflater == null)
             inflater = (LayoutInflater) activity
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         if (convertView == null)
-            convertView = inflater.inflate(R.layout.list_row, null);
-        /*if (convertView == null) {
-            convertView = inflater.inflate(R.layout.list_row, parent, false);
-        }
-*/
+            convertView = inflater.inflate(R.layout.volley_list_row, null);
+
         if (imageLoader == null)
             imageLoader = AppController.getInstance().getImageLoader();
         NetworkImageView thumbNail = (NetworkImageView) convertView
@@ -63,19 +60,19 @@ public class CustomListAdapter extends BaseAdapter {
         TextView rating = (TextView) convertView.findViewById(R.id.rating);
         TextView genre = (TextView) convertView.findViewById(R.id.genre);
         TextView year = (TextView) convertView.findViewById(R.id.releaseYear);
- 
+
         // getting movie data for the row
         Movie m = movieItems.get(position);
- 
+
         // thumbnail image
         thumbNail.setImageUrl(m.getThumbnailUrl(), imageLoader);
-         
+
         // title
         title.setText(m.getTitle());
-         
+
         // rating
         rating.setText("Rating: " + String.valueOf(m.getRating()));
-         
+
         // genre
         String genreStr = "";
         for (String str : m.getGenre()) {
@@ -84,11 +81,89 @@ public class CustomListAdapter extends BaseAdapter {
         genreStr = genreStr.length() > 0 ? genreStr.substring(0,
                 genreStr.length() - 2) : genreStr;
         genre.setText(genreStr);
-         
+
         // release year
         year.setText(String.valueOf(m.getYear()));
- 
+
         return convertView;
     }
- 
+
 }
+
+
+
+/*extends BaseAdapter {
+    private Activity activity;
+    private LayoutInflater inflater;
+    private List<Movie> movieItems;
+    ImageLoader imageLoader = AppController.getInstance().getImageLoader();
+
+    public CustomListAdapter(Activity activity, List<Movie> movieItems) {
+        this.activity = activity;
+        this.movieItems = movieItems;
+    }
+
+    @Override
+    public int getCount() {
+        return movieItems.size();
+    }
+
+    @Override
+    public Object getItem(int location) {
+        return movieItems.get(location);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (inflater == null)
+            inflater = (LayoutInflater) activity
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null)
+            convertView = inflater.inflate(R.layout.volley_list_row, null);
+        *//*if (convertView == null) {
+            convertView = inflater.inflate(R.layout.volley_list_row, parent, false);
+        }
+*//*
+        if (imageLoader == null)
+            imageLoader = AppController.getInstance().getImageLoader();
+        NetworkImageView thumbNail = (NetworkImageView) convertView
+                .findViewById(R.id.thumbnail);
+        TextView title = (TextView) convertView.findViewById(R.id.title);
+        TextView rating = (TextView) convertView.findViewById(R.id.rating);
+        TextView genre = (TextView) convertView.findViewById(R.id.genre);
+        TextView year = (TextView) convertView.findViewById(R.id.releaseYear);
+
+        // getting movie data for the row
+        Movie m = movieItems.get(position);
+
+        // thumbnail image
+        thumbNail.setImageUrl(m.getThumbnailUrl(), imageLoader);
+
+        // title
+        title.setText(m.getTitle());
+
+        // rating
+        rating.setText("Rating: " + String.valueOf(m.getRating()));
+
+        // genre
+        String genreStr = "";
+        for (String str : m.getGenre()) {
+            genreStr += str + ", ";
+        }
+        genreStr = genreStr.length() > 0 ? genreStr.substring(0,
+                genreStr.length() - 2) : genreStr;
+        genre.setText(genreStr);
+
+        // release year
+        year.setText(String.valueOf(m.getYear()));
+
+        return convertView;
+    }
+
+}*/
